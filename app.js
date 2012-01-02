48de9a680b48ec3814fb5b885f1a61c8d6b2b66a
Mike <falk@archcity.localdomain>
2012-01-02 16:27:38 -0600

    Server is go

---
+++ app.js
@@ -0,0 +1,54 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , Player = require('./public/javascripts/player.js')
+  , game = require('./game')
+
+var app = module.exports = express.createServer()
+  , io = require('socket.io').listen(app);
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+io.configure(function() {
+		// Only use WebSockets
+		io.set("transports", ["websocket"]);
+		 //Restrict log output
+		io.set("log level", 2);
+});
+
+// Routes
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+  });
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+//Socket Handling and Game logic
+
+game.gamestart(io);
+
+
+
+
